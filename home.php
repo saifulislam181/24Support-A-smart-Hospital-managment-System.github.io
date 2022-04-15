475a9f3047f58295ae5336a9fc46dfb376bcf8e4
Saiful <mislam181292@bscse.uiu.ac.bd>
2022-04-15 17:41:52 +0600

    Home.php commited

---
+++ home.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html>
+    <head>
+       <link href="home.css" rel="stylesheet">
+        <title>Nagorik</title>
+        <script src="https://kit.fontawesome.com/76235dc605.js" crossorigin="anonymous"></script>
+    <style type="text/css">
+        <link rel="icon" href="img/homeicon.png">
+        <style type="text/css">
+            a{
+                text-decoration: none;
+            }
+            .review{
+                text-align: center;
+                margin: 30px;
+                padding: 30px;
+            }
+            h1{
+                padding: 10px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="header">
+            <img src="img/Log2.jpg" class="logo-img">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="about.php">About Us</a></li>
+                <li><a href="donation.php">Donation</a></li>
+                <li><a href="organization.php">Organization</a></li>
+                <li><a href="shareRequest.php">Share Request</a></li>
+                <li><a href="area.php">Area</a></li>
+                <li><a href="event.php">Successful Event</a></li>
+                <li><a href="contact.php">Contact Us</a></li>
+
+            </ul>
+        </div>
+        
+<!--section 1 start-->
+
+        <div class="bg">
+            <div class="title">
+                <h1>Welcome to 24Support</h1>
+                <h2>using this site for make a simple life and harselless also save your time.</h2>
+            </div>
+         
+        </div>
+
+     <!--section 02-->
+
+        <div class="sec2">
+            <h1>Important Notice : !!!</h1>
+            <div class="border-bottom"></div>
+            <p>A e report and managment system.</p>
+            
+       </div>
+
+        <!--section 03-->
+        <div class="sec3">
+            <h1>Successful Events</h1>
+        
+        
+            <div class="flip-flex">
+
+            <div class="container1">
+                <div class="flip-card1">
+                    <div class="font">
+                        <img src="img/bld.jpg" width="350px" height="250px">
+                    </div>
+                    <div class="back"><h3>Blood Donation</h3>
+                    
+                        <p>Thankyou Joyshree Sarkar !!! Your blood donation was successfully 02.02.2022.Just awsome for donation your blood was save a mother and a child.Best of luck and again thankyou.!!!</p>
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="container2">
+                <div class="flip-card2">
+                    <div class="font">
+                        <img src="img/patient.jpg" width="350px" height="250px">
+                    </div>
+                    <div class="back">
+                        <h3>Save a life</h3>
+                        <p>Thankyou all!!Your small contribution can same a life.In last 30.02.21 successfully done Rahim mia Operation.The total of donation was 3.2lakh.The hospital bill was 2.8 lakh and rest of the amount was given him.It was just awsome for helping people</p>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="container3">
+                <div class="flip-card3">
+                    <div class="font">
+                        <img src="img/patient2.jpg" width="350px" height="250px">
+                    </div>
+                    <div class="back">
+                        <h3>Save a life</h3>
+                        <p>Thankyou all!!Your small contribution can save a life.In last 14.07.21 successfully done Feroja Begam Operation.The total of donation was 80k .The hospital bill was 75k and rest of the amount was given her.It was just awsome for helping people</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+        
+
+               
+
+
+        <!--section 04-->
+        <div class="sec4">
+            <h1>Please Help Them</h1>
+
+            <div class="orga">
+                <div class="orga1">
+                    <h3>Md.Saiful Islam</h3>
+                    <p1>  A heart deises patient.   Who did not pay the hospital bill.The hospital discount 50% of bill.  In this situation he need to an Operation as soon as possible.  Your small contribution will save him life.    Ibn sina Hospital.Bed no 14. 
+					</p1>
+					<\n>
+                    <a href="moneydonation.php">Click Here</a>
+                </div>
+
+                <div class="orga2">
+                    <h3>Joyshree Sarkar</h3>
+                    <p>An serious patient. Who did not pay the hospital bill.The hospital discount 50% of bill. In this situation she needs to an Operation as soon as possible. Your small contribution will save her life. Ibn sina Hospital.Bed no 15.</p>
+                    <a href="moneydonation.php">Click Here</a>
+                </div>
+
+                <div class="orga3">
+                    <h3>Sreeja Dey</h3>
+                    <p>A mental patient who was out of her menatality by an accident.A major operation needs for her.But her family did not abale to pay the operation bill.Hospital have already 40% discount but need more then 3 lakhs tk.Pleae help her and save her life.Your small contribution whii be save a life.Ibn sina Hospital Bed no 16 </p>
+                    <a href="moneydonation.php">Click Here</a>
+                </div>
+
+            </div>
+        </div>
+
+
+        <div class="review">
+            <hr>
+            <a href="reviewlist.php">
+
+           <i class="fas fa-star-half-alt"></i> <h1>All Events Review</h1></a>
+
+            <hr>
+        </div>
+
+        <!--footer section-->
+        <div class="footer">
+            <h4>Copyright &copy:2021 |All right Reserved</h4>
+
+        </div>
+    </body>
+
+
+</html>
