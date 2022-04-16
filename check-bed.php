51f5a47c3babfa6ace61e5911909ee338d9565fc
Omor Nahid <omornahid@gmail.com>
2022-04-16 09:49:36 +0600

    added css for check-bed.php file

--- check-bed.php
+++ check-bed.php
@@ -3,6 +3,62 @@
 <head>
 	<title>Bed List</title>
 	<script src="https://kit.fontawesome.com/76235dc605.js" crossorigin="anonymous"></script>
+	<style type="text/css">
+		*{
+			margin: 0;
+			padding: 0;
+			font-family: cursive;
+		}
+
+		a{
+			text-decoration: none;
+		}
+
+		.section1{
+			text-align: center;
+			color: #C0E38E;
+			background-color: #05A966;
+			padding:5px;
+		}
+
+		.table1{
+			border:2px double #E5EB2E;
+			margin-top: 20px;
+			margin-left: auto;
+			margin-right: auto;
+			border-collapse: collapse;
+			margin-bottom: 50px;
+			text-align: center;
+		}
+
+		td,th{
+			border:1px solid tomato;
+			text-align: justify;
+			padding: 10px;
+		}
+		tr:nth-child(odd){
+			background-color: #ececec;
+		}
+		.back{
+			background: #05A966;
+			text-align: center;
+			padding: 15px;
+			border-radius: 40px;
+			margin-left: 550px;
+			font-size: 15px;
+			font-weight: bold;
+			color: yellow;
+
+		}
+		.back:hover{
+			background:white;
+			color: #05A966;
+			border:1px solid #E5EB2E;
+		}
+		.back i{
+			margin-right: 5px;
+		}
+	</style>
 	</head>
 <body>
 
