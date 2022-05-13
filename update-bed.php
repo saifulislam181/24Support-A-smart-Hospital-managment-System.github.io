510ace851d3098cc3d32be3cb9e1e0246134cc14
Omor Nahid <omornahid@gmail.com>
2022-05-14 02:03:48 +0600

    write html code to create table to fetch data

--- update-bed.php
+++ update-bed.php
@@ -2,3 +2,47 @@
 include('config.php');
 $result = mysqli_query($conn,"SELECT * FROM availablebed");
 ?>
+
+<!DOCTYPE html>
+<html>
+ <head>
+   <title> Read data</title>
+   <link rel="stylesheet" href="style.css">
+ </head>
+<body>
+<?php
+if (mysqli_num_rows($result) > 0) {
+?>
+<table>
+      <tr>
+        <td>#</td>
+        <td>Floor</td>
+        <td>Block</td>
+        <td>Room</td>
+        <td>Bed</td>
+        <td>Action</td>
+      </tr>
+            <?php
+            $i=0;
+            while($row = mysqli_fetch_array($result)) {
+            ?>
+      <tr>
+        <td><?php echo $row["id"]; ?></td>
+        <td><?php echo $row["floor"]; ?></td>
+        <td><?php echo $row["block"]; ?></td>
+        <td><?php echo $row["room"]; ?></td>
+        <td><?php echo $row["bed"]; ?></td>
+        <td><a href="update-process.php?id=<?php echo $row["id"]; ?>">Update</a></td>
+      </tr>
+            <?php
+            $i++;
+            }
+            ?>
+</table>
+ <?php
+}
+else
+{
+    echo "No result found";
+}
+?>
