48994ddd663e1e342d38c85a483698b1702aed51
sreeja <sreejadey1997@gmail.com>
2022-05-14 14:04:35 +0600

    Commited submit

--- blooddonation.php
+++ blooddonation.php
@@ -113,6 +113,7 @@ if(isset($_REQUEST['submit']))
            
             </form>
         </div>
+        <p id="msgHolder">Please fill up all the information for submit your Donation</p>
         </div>
 
        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
