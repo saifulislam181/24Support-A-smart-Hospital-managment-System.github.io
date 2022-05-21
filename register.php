aaf05173ac40f6c7b612cecd82ee6eaf59aa7f64
joyshree <jsarkar181169@bscse.uiu.ac.bd>
2022-05-21 12:12:13 +0600

    commited register

---
+++ register.php
@@ -0,0 +1,30 @@
+<?php                     
+    if (isset($_POST["register"])) {
+        $connection = new mysqli("localhost", "root", "", "24support");
+
+		$firstName = $connection->real_escape_string($_POST["firstName"]);  		
+		$lastName = $connection->real_escape_string($_POST["lastName"]);  				
+		$email = $connection->real_escape_string($_POST["email"]);  
+		$password = sha1($connection->real_escape_string($_POST["password"])); 
+			
+		$data = $connection->query("INSERT INTO register (firstName, lastName, email, password) VALUES ('$firstName', '$lastName', '$email', '$password')");
+
+    	if ($data === false)
+        	echo "Connection error!";
+    	else
+    	   echo "Your have been signed up - please now Log In";
+	}	                 
+?>
+
+
+<html>
+    <body>
+        <form method="post" action="register.php">      
+            <input type="text" name="firstName" placeholder="Your First Name"  />        
+            <input type="text" name="lastName" placeholder="Your Last Name"  />        
+            <input type="email" name="email" placeholder="Your Email"  />
+            <input type="password" name="password" placeholder="Password"  />
+            <input type="submit" name="register" value="Register" />                   
+        </form>
+    </body>
+</html>
